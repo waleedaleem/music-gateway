b84fb94060bd8a2b9d320e14a8e071c48b3fd5e2
Walid Moustafa <waleedaleem@hotmail.com>
2020-09-05 13:57:18 +1000

    Configure the Zuul route to musicbrainz.org

--- src/main/java/com/walid/music/MusicGatewayApplication.java
+++ src/main/java/com/walid/music/MusicGatewayApplication.java
@@ -2,8 +2,14 @@ package com.walid.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication public class MusicGatewayApplication {
+/**
+ * @author wmoustaf
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class MusicGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicGatewayApplication.class, args);
