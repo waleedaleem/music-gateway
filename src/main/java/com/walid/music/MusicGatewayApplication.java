d3f15af45997abc753aa5cc680f85e3784a34fbb
Walid Moustafa <waleedaleem@hotmail.com>
2020-09-05 12:32:59 +1000

    Initial commit

---
+++ src/main/java/com/walid/music/MusicGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.walid.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class MusicGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walid/music/MusicGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.walid.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class MusicGatewayApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
